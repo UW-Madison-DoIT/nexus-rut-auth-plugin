40d4159240b9116b9afecfe34c09b487fc3575f1
Eric Dalquist <eric.dalquist@gmail.com>
2012-05-01 11:01:53 -0500

    Update JS inclusion approach

--- nexus-rut-auth-plugin/src/main/java/edu/wisc/nexus/auth/rut/realm/RemoteUserTokenIndexHtmlCustomizer.java
+++ nexus-rut-auth-plugin/src/main/java/edu/wisc/nexus/auth/rut/realm/RemoteUserTokenIndexHtmlCustomizer.java
@@ -19,9 +19,17 @@
 
 package edu.wisc.nexus.auth.rut.realm;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
 import org.codehaus.plexus.component.annotations.Component;
+import org.sonatype.guice.bean.reflect.ClassSpace;
+import org.sonatype.guice.bean.reflect.URLClassSpace;
 import org.sonatype.nexus.plugins.rest.AbstractNexusIndexHtmlCustomizer;
 import org.sonatype.nexus.plugins.rest.NexusIndexHtmlCustomizer;
 
@@ -31,19 +39,65 @@ import org.sonatype.nexus.plugins.rest.NexusIndexHtmlCustomizer;
  */
 @Component( role = NexusIndexHtmlCustomizer.class, hint = "RemoteUserTokenIndexHtmlCustomizer" )
 public class RemoteUserTokenIndexHtmlCustomizer extends AbstractNexusIndexHtmlCustomizer {
-    @Override
-    public String getPostHeadContribution(Map<String, Object> ctx) {
-        String scriptName = "nexus-rut-auth-plugin-all.min.js";
+    public static final String PROP_FILE_NAME = "/" + RemoteUserTokenIndexHtmlCustomizer.class.getSimpleName() + ".properties";
+    
+    private final String scriptTag;
+
+    public RemoteUserTokenIndexHtmlCustomizer() {
+        final InputStream propStream = getClass().getResourceAsStream(PROP_FILE_NAME);
+        if (propStream == null) {
+            throw new IllegalStateException("Could not find " + PROP_FILE_NAME + " on classpath");
+        }
+
+        final Properties props = new Properties();
+        try {
+            props.load(propStream);
+        }
+        catch (IOException e) {
+            throw new IllegalStateException("Could not load " + PROP_FILE_NAME + " from classpath", e);
+        }
+        finally {
+            IOUtils.closeQuietly(propStream);
+        }
         
-        String version = getVersionFromJarFile("/META-INF/maven/edu.wisc/nexus-rut-auth-plugin/pom.properties");
+        final String groupId = props.getProperty("groupId");
+        final String artifactId = props.getProperty("artifactId");
+        final String minifiedJs = props.getProperty("minifiedJs");
+        
+        final StringBuilder scriptTagBuilder = new StringBuilder();
+        String version = getVersionFromJarFile("/META-INF/maven/" + groupId + "/" + artifactId + "/pom.properties");
         if (version != null && version.endsWith("-SNAPSHOT")) {
             //Get around caching on snapshot dev
             version = Long.toString(System.currentTimeMillis());
-            scriptName = "nexus-rut-auth-plugin-all.js";
+            
+            //Search the classpath for scripts
+            final ClassSpace classSpace = new URLClassSpace(this.getClass().getClassLoader());
+            for (final Enumeration<URL> entries = classSpace.findEntries("/static/js/", artifactId + "-*.js", true);
+                    entries.hasMoreElements();) {
+                
+                final URL script = entries.nextElement();
+                final String path = script.getPath();
+                final String file = path.substring(path.lastIndexOf('/') + 1);
+
+                if (!file.equals(minifiedJs)) {
+                    addScript(scriptTagBuilder, version, file);
+                }
+            }
+        }
+        else {
+            addScript(scriptTagBuilder, version, minifiedJs);
         }
 
-        return "<script src=\"static/js/" + scriptName + (version == null ? "" : "?" + version)
-                + "\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+        this.scriptTag = scriptTagBuilder.toString();
+    }
 
+    protected void addScript(final StringBuilder scriptTags, String version, final String file) {
+        scriptTags.append("<script src=\"static/js/").append(file).append("?").append(version)
+                .append("\" type=\"text/javascript\" charset=\"utf-8\"></script>\n");
+    }
+
+    @Override
+    public String getPostHeadContribution(Map<String, Object> ctx) {
+        return this.scriptTag;
     }
 }
